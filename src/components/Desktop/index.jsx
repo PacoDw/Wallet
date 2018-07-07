a04e6467e41a445ebe69031906d6c8fc9c5bbde8
Paco Preciado Mendoza <__pm@outlook.com>
2018-07-07 16:35:40 -0500

    :art: Add request api

--- src/components/Desktop/index.jsx
+++ src/components/Desktop/index.jsx
@@ -1,141 +1,140 @@
 import React, { Component } from 'react';
-import Header from '../Header'
-import SideDrawer from '../Sidedrawer'
-import Modal from '../Modal';
+
+// Components--------------------------------------
+import Header 		from '../Header'
+import SideDrawer 	from '../Sidedrawer'
+import Modal 		from '../Modal';
 import FusionCharts from 'fusioncharts';
-import Charts from 'fusioncharts/fusioncharts.charts';
-import ReactFC from 'react-fusioncharts';
+import Charts       from 'fusioncharts/fusioncharts.charts';
+import ReactFC      from 'react-fusioncharts';
+
 
 Charts(FusionCharts);
 
 let myDataSource = {
-  chart: {
-    caption: 'Harry\'s SuperMart',
-    subCaption: 'Top 5 stores in last month by revenue',
-    numberPrefix: '$',
-  },
-  data: [
-    {
-      label: 'Bakersfield Central',
-      value: '880000',
-    },
-    {
-      label: 'Garden Groove harbour',
-      value: '730000',
-    },
-    {
-      label: 'Los Angeles Topanga',
-      value: '59000',
-    },
-    {
-      label: 'Compton-Rancho Dom',
-      value: '520000',
-    },
-    {
-      label: 'Daly City Serramonte',
-      value: '330000',
-    },
-  ],
+	chart: {
+		caption: 'Harry\'s SuperMart',
+		subCaption: 'Top 5 stores in last month by revenue',
+		numberPrefix: '$',
+	},
+	data: [
+		{
+			label: 'Bakersfield Central',
+			value: '880000',
+		},
+		{
+			label: 'Garden Groove harbour',
+			value: '730000',
+		},
+		{
+			label: 'Los Angeles Topanga',
+			value: '59000',
+		},
+		{
+			label: 'Compton-Rancho Dom',
+			value: '520000',
+		},
+		{
+			label: 'Daly City Serramonte',
+			value: '330000',
+		},
+	],
 };
 
 const chartConfigs = {
-  type: 'pie3d',
-  width: "80%",
-  height: "400",
-  dataFormat: 'json',
-  dataSource: myDataSource,
+	type: 'pie3d',
+	width: "80%",
+	height: "400",
+	dataFormat: 'json',
+	dataSource: myDataSource,
 };
 
 class index extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			showModal: false,
+			option: '',
+			value: ''
+		}
+
+		this.handleShowSideDrawer = this.handleShowSideDrawer.bind(this);
+		this.handleHideSideDrawer = this.handleHideSideDrawer.bind(this);
+		this.handleTittle 		  = this.handleTittle.bind(this);
+
+		this.handleShowModal      = this.handleShowModal.bind(this);
+		this.handleCloseModal     = this.handleCloseModal.bind(this);
+
+		this.handleGetValueInput  = this.handleGetValueInput.bind(this);
+	}
+
+	handleGetValueInput(e) {
+		e.preventDefault();
+		this.setState({ value: e.target.value });
+	}
+
+	handleShowModal(e) {
+		e.preventDefault();
+		this.setState({ showModal: true, option: e.target.text });
+
+	}
+	handleCloseModal(e) {
+		e.preventDefault();
+		this.setState({ showModal: false });
+	}
 
 
-  constructor(props) {
-    super(props);
+	handleShowSideDrawer(e) {
+		e.preventDefault();
+		let sidedrawer = document.querySelector('#sidedrawer')
+		setTimeout(function () {
+			sidedrawer.classList.add('active');
+		}, 20);
+	}
 
-    this.state = {
-      showModal: false,
-      option: '',
-      value: ''
-    }
+	handleHideSideDrawer(e) {
+		e.preventDefault();
+		document.querySelector('body').classList.toggle('hide-sidedrawer');
+	}
 
-    this.handleShowSideDrawer = this.handleShowSideDrawer.bind(this);
-    this.handleHideSideDrawer = this.handleHideSideDrawer.bind(this);
-    this.handleTittle = this.handleTittle.bind(this);
+	handleTittle(e) {
+		let ul = e.target.nextSibling;
 
-    this.handleShowModal = this.handleShowModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
+		ul.classList.toggle('hiden-items');
+	}
 
-    this.handleGetValueInput = this.handleGetValueInput.bind(this);
-  }
+	render() {
+		return (
+			<div>
+				<SideDrawer
+					handleTittle={this.handleTittle}
+					handleShowModal={this.handleShowModal}
+				/>
 
-  handleGetValueInput(e) {
-    e.preventDefault();
-    this.setState({ value: e.target.value });
-  }
+				<Header
+					showSidedrawer={this.handleShowSideDrawer}
+					hideSidedrawer={this.handleHideSideDrawer}
+				/>
+				<div id="content-wrapper">
+					<div class="mui--appbar-height"></div>
+					<div class="mui-container-fluid">
+						<div className='mui-panel'>
+							<Modal
+								option={this.state.option}
+								showModal={this.state.showModal}
+								handleCloseModal={this.handleCloseModal}
+							/>
 
-  handleShowModal(e) {
-    e.preventDefault();
-    this.setState({ showModal: true, option: e.target.text });
+							<ReactFC {...chartConfigs}// Provide FusionCharts library
+							/>
 
-  }
-  handleCloseModal(e) {
-    e.preventDefault();
-    this.setState({ showModal: false });
-  }
-
-
-  handleShowSideDrawer(e) {
-    e.preventDefault();
-    let sidedrawer = document.querySelector('#sidedrawer')
-    console.log("entro");
-    setTimeout(function () {
-      sidedrawer.classList.add('active');
-    }, 20);
-  }
-
-  handleHideSideDrawer(e) {
-    e.preventDefault();
-    console.log("entro");
-    document.querySelector('body').classList.toggle('hide-sidedrawer');
-  }
-
-  handleTittle(e) {
-    let ul = e.target.nextSibling;
-
-    ul.classList.toggle('hiden-items');
-  }
-
-  render() {
-    console.log('Modal : ', this.state.showModal);
-
-    return (
-      <div>
-        <SideDrawer
-          handleTittle={this.handleTittle}
-          handleShowModal={this.handleShowModal}
-        />
-
-        <Header
-          showSidedrawer={this.handleShowSideDrawer}
-          hideSidedrawer={this.handleHideSideDrawer}
-        />
-        <div id="content-wrapper">
-          <div class="mui--appbar-height"></div>
-          <div class="mui-container-fluid">
-            <div className='mui-panel'>
-              <Modal
-                showModal={this.state.showModal}
-                handleCloseModal={this.handleCloseModal}
-                option={this.state.option}
-              />
-              <ReactFC {...chartConfigs}// Provide FusionCharts library
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default index;
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,19 +5,72 @@ import ReactModal from 'react-modal';
 import AddIncome  from './AddIncome';
 import AddOutcome from './AddOutcome'
 
+import Api        from '../../utils';
+import onGetForm  from '../../utils/onGetForm';
+
 const customStyles = {
     content : {
-    
       top          : '50%',
       left         : '50%',
       right        : 'auto',
       bottom       : 'auto',
       marginRight  : '-50%',
+      width        : '35%',
       transform    : 'translate(-50%, -50%)'
     }
   };
 
-export default class Modal extends React.Component {
+class Modal extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.handleAddIncome  = this.handleAddIncome.bind(this);
+        this.handleAddOutcome = this.handleAddOutcome.bind(this); 
+    }
+
+    handleAddOutcome(e){
+        e.preventDefault();
+
+        let formulario = document.querySelector('#AddOutcome');
+
+        console.log('Target: ', formulario);
+
+		onGetForm(formulario, ( dataForm ) => {
+                console.log('dataForm: ', dataForm);
+                alert('Paro');
+                
+			Api.Outcome
+				.addOutcome( dataForm, 1 )
+
+				.then( data => {
+                    console.log(data)
+					// this.setState( { user : data.user} )
+                })
+                
+                .catch(error => console.log(error) );
+		})
+
+    }
+
+    handleAddIncome(e){
+        e.preventDefault();
+
+        let formulario = document.querySelector('#AddIncome');
+
+        onGetForm(formulario, ( dataForm ) => {
+                
+            Api.Income
+                .addIncome( dataForm )
+
+                .then( data => {
+                    console.log(data)
+                    // this.setState( { user : data.user} )
+                })
+                
+                .catch(error => console.log(error) );
+        })
+    }
+    
     render() {
         const { showModal, handleCloseModal, option } = this.props;
 
@@ -26,12 +79,16 @@ export default class Modal extends React.Component {
 
         if ( option === 'Agregar gasto')
         {
-            typeModal = <AddOutcome/>
+            typeModal = <AddOutcome
+                            AddOutcome = { this.handleAddOutcome }
+                        />
             title     = 'Agregar Gasto'
         }
         else
         {
-            typeModal = <AddIncome/>
+            typeModal = <AddIncome
+                            AddIncome = { this.handleAddIncome }
+                        />
             title     = 'Agregar Ingreso'
         }
 
@@ -50,5 +107,5 @@ export default class Modal extends React.Component {
             </ReactModal>
         );
     }
-
 }
+export default Modal;
