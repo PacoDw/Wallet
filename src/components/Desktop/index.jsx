c5693503f5dabb2b1b893021293cdba0386a06fc
Paco Preciado Mendoza <__pm@outlook.com>
2018-07-07 05:15:13 -0500

    :ok_hand: Add Some Style

--- src/components/Desktop/index.jsx
+++ src/components/Desktop/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header     from '../Header'
+import Header from '../Header'
 import SideDrawer from '../Sidedrawer'
-import Modal      from '../Modal';
+import Modal from '../Modal';
 import FusionCharts from 'fusioncharts';
 import Charts from 'fusioncharts/fusioncharts.charts';
 import ReactFC from 'react-fusioncharts';
@@ -9,133 +9,133 @@ import ReactFC from 'react-fusioncharts';
 Charts(FusionCharts);
 
 let myDataSource = {
-    chart: {
-      caption: 'Harry\'s SuperMart',
-      subCaption: 'Top 5 stores in last month by revenue',
-      numberPrefix: '$',
+  chart: {
+    caption: 'Harry\'s SuperMart',
+    subCaption: 'Top 5 stores in last month by revenue',
+    numberPrefix: '$',
+  },
+  data: [
+    {
+      label: 'Bakersfield Central',
+      value: '880000',
     },
-    data: [
-      {
-        label: 'Bakersfield Central',
-        value: '880000',
-      },
-      {
-        label: 'Garden Groove harbour',
-        value: '730000',
-      },
-      {
-        label: 'Los Angeles Topanga',
-        value: '59000',
-      },
-      {
-        label: 'Compton-Rancho Dom',
-        value: '520000',
-      },
-      {
-        label: 'Daly City Serramonte',
-        value: '330000',
-      },
-    ],
-  };
-  
-  const chartConfigs = {
-    type: 'pie3d',
-    width: "80%",
-    height: "400",
-    dataFormat: 'json',
-    dataSource: myDataSource,
-  };
-  
+    {
+      label: 'Garden Groove harbour',
+      value: '730000',
+    },
+    {
+      label: 'Los Angeles Topanga',
+      value: '59000',
+    },
+    {
+      label: 'Compton-Rancho Dom',
+      value: '520000',
+    },
+    {
+      label: 'Daly City Serramonte',
+      value: '330000',
+    },
+  ],
+};
+
+const chartConfigs = {
+  type: 'pie3d',
+  width: "80%",
+  height: "400",
+  dataFormat: 'json',
+  dataSource: myDataSource,
+};
+
 class index extends Component {
 
 
-    constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			showModal : false,
-			option    : '',
-			value     : ''
-		}
-
-    this.handleShowSideDrawer  = this.handleShowSideDrawer.bind(this);
-    this.handleHideSideDrawer  = this.handleHideSideDrawer.bind(this);
-    this.handleTittle          = this.handleTittle.bind(this);
-		
-		this.handleShowModal = this.handleShowModal.bind(this);
-		this.handleCloseModal = this.handleCloseModal.bind(this);
-
-		this.handleGetValueInput = this.handleGetValueInput.bind(this);
-	}
-
-	handleGetValueInput(e) {
-        e.preventDefault();
-        this.setState( { value : e.target.value } );
+    this.state = {
+      showModal: false,
+      option: '',
+      value: ''
     }
 
-	handleShowModal(e) {
-		e.preventDefault();
-		this.setState( { showModal : true, option : e.target.text } );
+    this.handleShowSideDrawer = this.handleShowSideDrawer.bind(this);
+    this.handleHideSideDrawer = this.handleHideSideDrawer.bind(this);
+    this.handleTittle = this.handleTittle.bind(this);
 
-	}
-	handleCloseModal (e) {
-		e.preventDefault();
-		this.setState( { showModal : false } );
-	}
-	
+    this.handleShowModal = this.handleShowModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
 
-    handleShowSideDrawer(e) {
-        e.preventDefault();
-        let sidedrawer = document.querySelector('#sidedrawer')
-       console.log("entro");
-        setTimeout(function () {
-            sidedrawer.classList.add('active');
-        }, 20);
-    }
+    this.handleGetValueInput = this.handleGetValueInput.bind(this);
+  }
 
-    handleHideSideDrawer(e){
-        e.preventDefault();
-        console.log("entro");
-        document.querySelector('body').classList.toggle('hide-sidedrawer');
-    }
+  handleGetValueInput(e) {
+    e.preventDefault();
+    this.setState({ value: e.target.value });
+  }
 
-    handleTittle(e) {		
-		let ul = e.target.nextSibling;
+  handleShowModal(e) {
+    e.preventDefault();
+    this.setState({ showModal: true, option: e.target.text });
 
-		ul.classList.toggle('hiden-items');
-    } 
-    
-    render() {
-		console.log('Modal : ', this.state.showModal);
+  }
+  handleCloseModal(e) {
+    e.preventDefault();
+    this.setState({ showModal: false });
+  }
 
-        return (
-            <div>
-                <SideDrawer
-					handleTittle    = { this.handleTittle }
-					handleShowModal = { this.handleShowModal } 
-                />
 
-				<Header 
-					showSidedrawer = { this.handleShowSideDrawer }
-					hideSidedrawer = { this.handleHideSideDrawer }
-				/>
-                <div id="content-wrapper">
-					<div class="mui--appbar-height"></div>
-						<div class="mui-container-fluid">
-						<div className='mui-panel'>
-							<Modal 
-								showModal        = { this.state.showModal  }
-								handleCloseModal = { this.handleCloseModal }
-								option           = { this.state.option }
-							/>
-                            <ReactFC {...chartConfigs}// Provide FusionCharts library
-                            />
-						</div>
-					</div>
-				</div>
-			</div>
-        );
-    }
+  handleShowSideDrawer(e) {
+    e.preventDefault();
+    let sidedrawer = document.querySelector('#sidedrawer')
+    console.log("entro");
+    setTimeout(function () {
+      sidedrawer.classList.add('active');
+    }, 20);
+  }
+
+  handleHideSideDrawer(e) {
+    e.preventDefault();
+    console.log("entro");
+    document.querySelector('body').classList.toggle('hide-sidedrawer');
+  }
+
+  handleTittle(e) {
+    let ul = e.target.nextSibling;
+
+    ul.classList.toggle('hiden-items');
+  }
+
+  render() {
+    console.log('Modal : ', this.state.showModal);
+
+    return (
+      <div>
+        <SideDrawer
+          handleTittle={this.handleTittle}
+          handleShowModal={this.handleShowModal}
+        />
+
+        <Header
+          showSidedrawer={this.handleShowSideDrawer}
+          hideSidedrawer={this.handleHideSideDrawer}
+        />
+        <div id="content-wrapper">
+          <div class="mui--appbar-height"></div>
+          <div class="mui-container-fluid">
+            <div className='mui-panel'>
+              <Modal
+                showModal={this.state.showModal}
+                handleCloseModal={this.handleCloseModal}
+                option={this.state.option}
+              />
+              <ReactFC {...chartConfigs}// Provide FusionCharts library
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default index;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,12 +6,15 @@ const Header = ({ showSidedrawer, hideSidedrawer }) => {
         <header id="header">
             <div className="mui-appbar mui--appbar-line-height">
                 <div className="mui-container-fluid">
-                    <a onClick={showSidedrawer} className="sidedrawer-toggle mui--visible-xs-inline-block mui--visible-sm-inline-block js-show-sidedrawer">☰</a>
-                    <a onClick={hideSidedrawer} className="sidedrawer-toggle mui--hidden-xs mui--hidden-sm js-hide-sidedrawer">☰</a>
-                    <h1 style={{display: 'inline', textAlign: 'center'}}>My Wallet</h1>
+                    <a onClick = { showSidedrawer } className="sidedrawer-toggle mui--visible-xs-inline-block mui--visible-sm-inline-block js-show-sidedrawer">☰</a>
+                    <a onClick = { hideSidedrawer } className="sidedrawer-toggle mui--hidden-xs mui--hidden-sm js-hide-sidedrawer">☰</a>
                     <span className="mui--text-title mui--visible-xs-inline-block">My Wallet</span>
                 </div>
             </div>
+            <nav id='nav-header'>
+                <h3 id='nav-title'>My Wallet</h3>
+                <a id='nav-sesion'>Cerrar Sesión</a>
+            </nav>
         </header>
     );
 };
