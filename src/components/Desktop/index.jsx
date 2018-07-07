30b0a1d80ca55004053faed827a30a09fc95501b
Gustavo Morentin <gmorentin@ucol.mx>
2018-07-07 00:24:29 -0500

    Add wrapper in Desktop

--- src/components/Desktop/index.jsx
+++ src/components/Desktop/index.jsx
@@ -44,8 +44,31 @@ class index extends Component {
 					showSidedrawer = { this.handleShowSideDrawer }
 					hideSidedrawer = { this.handleHideSideDrawer }
 				/>
-
-            </div>
+                <div id="content-wrapper">
+					<div class="mui--appbar-height"></div>
+						<div class="mui-container-fluid">
+						<div className='mui-panel'>
+							<br/>
+							<h1>My Wallet</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+								sollicitudin volutpat molestie. Nullam id tempor nulla. Aenean sit amet
+								urna et elit pharetra consequat. Aliquam fringilla tortor vitae lectus
+								tempor, tempor bibendum nunc elementum. Etiam ultrices tristique diam,
+								vitae sodales metus bibendum id. Suspendisse blandit ligula eu fringilla
+								pretium. Mauris dictum gravida tortor eu lacinia. Donec purus purus,
+								ornare sit amet consectetur sed, dictum sitamet ex. Vivamus sit amet
+								imperdiet tellus. Quisque ultrices risus a massa laoreet, vitae tempus sem
+								congue. Maecenas nec eros ut lectus vehicula rutrum. Donec consequat
+								tincidunt arcu non faucibus. Duis elementum, ante venenatis lacinia
+								cursus, turpis massa congue magna, sed dapibus felis nibh sed tellus. Nam
+								consectetur non nibh vitae sodales. Pellentesque malesuada dolor nec mi
+								volutpat, eget vehicula eros ultrices.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
         );
     }
 }
