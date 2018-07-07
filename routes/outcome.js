974bc900b1ad71eca311da558578e7d42007f64c
ocastrejon <ocastrejon@ucol.mx>
2018-07-07 12:22:06 -0500

    :ok_hand: Update Outcome and Income

--- routes/outcome.js
+++ routes/outcome.js
@@ -37,7 +37,7 @@ router
 					req.body.amount,
 					req.body.description,
 					req.body.date,
-					req.body.id_frecuency,
+					req.body.id_frequency,
 					req.params.id
 				]	
 				
@@ -48,6 +48,43 @@ router
 						res.status(500).json({ err });
 					} else {
 						res.status(200).json({ Movement: 'Added' });
+					}
+				})
+			})
+
+
+			.get('/getOutcome/:id', function (req, res, next) {
+				const db = require('../database/config');	
+				let id = req.params.id;
+				db.query("select * from movements where movement_type LIKE '%Gasto%' && id_wallet = ?",id , (err, rows, fields) => {
+					console.log(rows);
+					if (err) {
+						res.status(500).json({ err });
+					} else {
+						res.status(200).json({ ok: rows });
+					}
+				})
+			})
+
+
+			.post('/editOutcome/:id', (req, res, next) => {
+				let movement = {
+					id_movement : req.params.id,
+					movement_type : req.body.movement_type,
+					amount : req.body.amount,
+					description : req.body.description,
+					date : 	req.body.date,
+					id_frequency : req.body.id_frequency,
+					id_wallet : req.body.id_wallet
+				}
+				
+				const db = require('../database/config');
+					db.query('UPDATE movements set ? where ?', [movement, {id_movement: req.params.id}], (err, rows, fields) => {
+					if (err) {
+						console.log(err);
+						res.status(500).json({ err });
+					} else {
+						res.status(200).json({ Movement: 'Updated' });
 					}
 				})
 			})
