ff47df6b74ae7bbdcd63bebc1e73cbf92266ceed
Gustavo Morentin <gmorentin@ucol.mx>
2018-07-08 15:15:42 -0500

    changes Identation

--- routes/settings.js
+++ routes/settings.js
@@ -1,35 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-router
-
-			.get('/', function (req, res, next) {
-				res.status(200).json({ Settings: 'Settings' });
-            })
-            
+router      
 			
 			.get('/profile/:id', (req, res, next) => {
-				let id = req.params.id;
-				const db = require('../database/config');
-				db.query('Select * from users where id_user = ?', id, (err, rows, fields) => {
-					if (err) {
-						res.status(500).json( err );
-					} else {
-						res.status(200).json( rows );
-					}
-				})
+
+					let id = req.params.id;
+					const db = require('../database/config');
+
+					db.query('Select * from users where id_user = ?', id, 
+						
+						(err, rows, fields) => {
+						
+							if (err) {
+								res.status(500).json( err );
+							} else {
+								res.status(200).json( rows );
+							}
+					})
 			})
 
 
 			.post('/resetApp/:id', (req, res, next) => {
-				let id = req.params.id;
-				const db = require('../database/config');
-				db.query('delete from movements where id_user = ?', id, (err, rows, fields) => {
-					if (err) {
-						res.status(500).json( err );
-					} else {
-						res.status(200).json({ user: 'Reseted' });
-					}
-				})
+
+					let id = req.params.id;
+					const db = require('../database/config');
+
+					db.query('delete from movements where id_user = ?', id, 
+					
+						(err, rows, fields) => {
+						
+							if (err) {
+								res.status(500).json( err );
+							} else {
+								res.status(200).json({ user: 'Reseted' });
+							}
+					})
 			})
 module.exports = router;
